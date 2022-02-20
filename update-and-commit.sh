1a8a95f1056e66759fbf3468e86f19f34eb861be
Joel <joelraymacsams@gmail.com>
2022-02-20 23:27:37 +0000

    Add update-and-commit.sh which updates data and commits the results automatically to Github

---
+++ update-and-commit.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Get the data for all the Sketchfab model pages being tracked, then commit and push.
+
+# set the project directory:
+project_directory="/home/$USER/projects/sketchfab-views-tracker/"
+
+cd $project_directory
+./sketchfab-views-tracker.sh
+cd data
+git add *.txt
+cd ..
+git commit -a -m "Automated fetch and commit of views data."
+git push
